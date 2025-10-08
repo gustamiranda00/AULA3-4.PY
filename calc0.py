c09e6abef44e9302897334153d3748b539964020
gustamiranda00 <gustavomiranda@colegiopaulodetarso.com.br>
2025-10-08 15:01:11 +0000

    git add

--- calc0.py
+++ calc0.py
@@ -7,15 +7,6 @@ print("A soma é: ", resultado)
 
 resultado = n1 * n2
 print("A multiplicação é: ", resultado)
- print("-----Calculadora-----")
-n1 = float(input("informe o primeiro número: "))
-n2 = float(input("informe o segundo número: "))
-
-resultado = n1 + n2
-print("A soma é: ", resultado)
-
-resultado = n1 * n2
-print("A multiplicação é: ", resultado)
 
 if n2 != 0:
     resultado = n1 / n2
